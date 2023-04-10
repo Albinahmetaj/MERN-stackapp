953398ec392b4a094ca1797646adb96a49e914d1
Albinahmetaj <albingithub@gmail.com>
2023-04-10 21:23:15 +0200

    workout Update req

--- frontend/src/components/WorkoutUpdateForm.js
+++ frontend/src/components/WorkoutUpdateForm.js
@@ -3,6 +3,7 @@ import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import { useParams } from "react-router-dom";
 import Multiselect from "multiselect-react-dropdown";
 import { createAPIEndpoint, ENDPOINTS } from "./axios";
+import axios from "axios";
 import {
   MenuItem,
   FormControl,
@@ -30,43 +31,46 @@ function WorkoutUpdateForm() {
   const [load, setLoad] = useState("");
   const [isActive, setIsActive] = useState(Boolean);
   const [error, setError] = useState(false);
-  const [proteinBrands, setProteinBrands] = useState([]);
+  const [myproteinBrands, setMyProteinBrands] = useState({
+    proteinBrands: [
+      {
+        name: [],
+        whey: Number,
+      },
+    ],
+  });
   const params = useParams();
   const [brands, setBrands] = useState([]);
 
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    proteinBrands.forEach((arr) =>
-      proteinBrands.forEach((obj) => {
-        if (obj.brands === value) {
-          setProteinBrands(obj.name);
-          console.log(arr);
-        }
-      })
-    );
+  const handleOptionsChange = (selectedOptions) => {
+    const updatedOptions = brands.map((option) => {
+      if (selectedOptions.includes(option._id)) {
+        return { ...option };
+      } else {
+        return { ...option };
+      }
+    });
+
+    setBrands(updatedOptions);
   };
 
-  const handleChangeWhey = (event) => {
-    const {
-      target: { value },
-    } = event;
-    proteinBrands.forEach((arr) =>
-      proteinBrands.forEach((obj) => {
-        if (obj.whey === value) {
-          setProteinBrands(
-            // On autofill we get a stringified value.
-            typeof value === Number ? value.split(",") : value
-          );
-          console.log(arr);
-        }
+  const handleSubmit = () => {
+    const updatedObject = {
+      ...myproteinBrands,
+      options: brands.filter((option) => option.brands),
+    };
+    axios
+      .put(`http://localhost:4000/api/workouts/${params.id}`, updatedObject)
+      .then((response) => {
+        console.log(response.data);
       })
-    );
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   console.log(
-    proteinBrands.forEach((arr) => {
+    myproteinBrands.forEach((arr) => {
       console.log(arr);
     })
   );
@@ -97,7 +101,13 @@ function WorkoutUpdateForm() {
       `http://localhost:4000/api/workouts/${params.id}`,
       {
         method: "PATCH",
-        body: JSON.stringify({ title, reps, load, isActive, proteinBrands }),
+        body: JSON.stringify({
+          title,
+          reps,
+          load,
+          isActive,
+          myproteinBrands,
+        }),
         header: {
           "Content-Type": "Application/json",
         },
@@ -142,19 +152,17 @@ function WorkoutUpdateForm() {
           />
 
           <label>Whey:</label>
-          <input
-            type="number"
-            onChange={handleChangeWhey}
-            value={proteinBrands}
-          />
+          <input type="number" />
           <FormControl sx={{ m: 1, width: 300 }}>
             <InputLabel id="demo-multiple-name-label">Brands</InputLabel>
             <Select
               labelId="demo-multiple-name-label"
               id="demo-multiple-name"
               multiple
-              value={proteinBrands}
-              onChange={handleChange}
+              value={brands
+                .filter((option) => option.brands)
+                .map((option) => option._id)}
+              onChange={handleOptionsChange}
               input={<OutlinedInput label="Name" />}
               MenuProps={MenuProps}
             >
@@ -171,7 +179,7 @@ function WorkoutUpdateForm() {
           <button
             style={{ display: "flex", marginTop: "20px" }}
             type="submit"
-            onClick={() => updateWorkout()}
+            onClick={handleSubmit}
           >
             Update workout
           </button>
